258ca6b2491bcab64edaba8fa4b4b8d530680fba
LucasKendi <hiranolucas@gmail.com>
2025-02-23 14:58:05 -0300

    Refactor

--- src/main.js
+++ src/main.js
@@ -6,17 +6,21 @@ function preload(){
 }
 
 function setup(){
+	start_time = Date.now()
 	setup_variables();
 	fixedImage();
 	getLayerInfo();
-	loop();
+	if(typeof img == 'undefined' || typeof copy == 'undefined' || !img.pixels || !copy.pixels) {
+		noLoop()
+	} else {
+		loop();
+	}
 }
 
 function setup_variables(){
 	range = 255 // max color value
-	layers = 20 // stl model layers
+	layers = 30 // stl model layers
 	layer = 1
-	base_color = 'black'
 	subd = parseInt(range/layers)
 	prev_color = []
 	dest_color = ''
@@ -39,37 +43,40 @@ function fixedImage() {
 }
 
 function draw() {
-  if(layer <= layers && colorsAt) {
-		if(typeof img !== 'undefined' && typeof copy !== 'undefined' && img.pixels && copy.pixels) {
-			let time = Date.now()
-			let thresh = parseInt(subd * (layer - 1)) // threshold to change pixels
-
-			for(i = 0; i < img.pixels.length; i += 4){
-				if (img.pixels[i + 3] == 0) continue; // skip on transparent pixels for png files
-
-				orig_color = layer == 1 ? color(base_color) : color(copy.pixels[i], copy.pixels[i + 1], copy.pixels[i + 2])
-				current_value = (img.pixels[i] + img.pixels[i + 1] + img.pixels[i + 2])/3
-
-				if(current_value >= thresh){
-					for(var cur_layer in colorsAt){
-						if(layer <= cur_layer) {
-							current_color = colorsAt[cur_layer]
-							break
-						}
-					}
-					color_layer(current_color)
-				}
-			}
-
-			console.log("Layer " + layer + ", with "+ current_color + ", taking: " + (Date.now() - time)/1000.0 + "s")
-			copy.updatePixels()
-			image(copy, 0, 0)
-			image(img, img.width + 20, 0)
-			layer++
-		}
-	} else {
-		noLoop()
+  if(layer > layers || !colorsAt) {
+		console.log("Total took "+ (Date.now() - start_time)/1000)
+		noLoop();
+		return;
 	}
+
+	let time = Date.now()
+	let thresh = parseInt(subd * (layer - 1)) // threshold to change pixels
+	let copyData = copy.pixels
+	let imgData = img.pixels
+
+	for (let cur_layer in colorsAt) {
+    if (layer <= cur_layer) {
+      targetColor = colorsAt[cur_layer];
+      break;
+    }
+  }
+
+	for(i = 0; i < imgData.length; i += 4){
+		if (imgData[i + 3] == 0) continue; // skip on transparent pixels for png files
+
+		current_value = (imgData[i] + imgData[i + 1] + imgData[i + 2])/3
+
+		if(current_value >= thresh){
+			orig_color = color(copyData[i], copyData[i + 1], copyData[i + 2])
+			color_layer(targetColor)
+		}
+	}
+
+	console.log("Layer " + layer + ", with "+ targetColor + ", taking: " + (Date.now() - time)/1000.0 + "s")
+	copy.updatePixels()
+	image(copy, 0, 0)
+	image(img, img.width + 20, 0)
+	layer++
 }
 
 function color_layer(cur_color) {
@@ -77,7 +84,7 @@ function color_layer(cur_color) {
 		prev_color = cur_color
 		dest_color = color(cur_color[0])
 	}
-  result = mixbox.lerp(orig_color.levels, dest_color.levels, cur_color[1]) // color mix using mixbox for accurate pigment mixing results
+	result = mixbox.lerp(orig_color.levels, dest_color.levels, cur_color[1]) // color mix using mixbox for accurate pigment mixing results
   copy.pixels[i + 0] = result[0]
   copy.pixels[i + 1] = result[1]
   copy.pixels[i + 2] = result[2]
