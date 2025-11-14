b598cb0b6c053589304ed8205c97c55b507aea0b
LucasKendi <hiranolucas@gmail.com>
2025-11-14 00:01:19 -0300

    small fixes

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ let time;
 let thresh;
 let copyData;
 let imgData;
-let currentLayer;
 let range = 255;
 let layers = 30;
 let layer = 1;
@@ -62,7 +61,7 @@ window.draw =() => {
   copyData = copy.pixels
   imgData = img.pixels
 
-  for (currentLayer in colorsAt) {
+  for (let currentLayer in colorsAt) {
     if (layer <= currentLayer) {
       targetColor = colorsAt[currentLayer];
       break;
@@ -87,12 +86,12 @@ window.draw =() => {
   layer++
 }
 
-function color_layer(cur_color) {
-  if (previousColor[0] != cur_color[0]) {
-    previousColor = cur_color
-    destColor = color(cur_color[0])
+function color_layer(currentColor) {
+  if (previousColor[0] != currentColor[0]) {
+    previousColor = currentColor
+    destColor = color(currentColor[0])
   }
-  let result = mixbox.lerp(origColor.levels, destColor.levels, cur_color[1]) // color mix using mixbox for accurate pigment mixing results
+  let result = mixbox.lerp(origColor.levels, destColor.levels, currentColor[1]) // color mix using mixbox for accurate pigment mixing results
   copy.pixels[i + 0] = result[0]
   copy.pixels[i + 1] = result[1]
   copy.pixels[i + 2] = result[2]
