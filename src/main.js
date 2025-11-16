65198eb6ed88f0d29c04dcebf42bb8c6b348d5da
LucasKendi <hiranolucas@gmail.com>
2025-11-15 23:05:50 -0300

    renaming variables

--- src/main.js
+++ src/main.js
@@ -1,44 +1,47 @@
 p5.disableFriendlyErrors = true; //small performance boost
 import { getLayerInfo, prepareImages, loadImageAsync } from './image_processor.js';
 
-let img;
+let origImage;
+let coloredImage;
 let range = 255;
 let layers = 20;
-let layer = 1;
+let currentLayer = 0;
 let subd = parseInt(range / layers);
 let previousColor = [];
 let destColor = '';
 let startTime;
 let colorsAt;
-let targetColor;
+let selectedColor;
 let currentValue;
 let origColor;
 let i;
 
 window.handleImageInput = async (event) => {
   let path = event.target.files[0];
+  event.target.value = "";
   if(!path) return;
 
-  img = await loadImageAsync(path);
-  copy = createImage(img.width, img.height)
-  copy.copy(img, 0, 0, img.width, img.height, 0, 0, copy.width, copy.height)
-  prepareImages(img, copy)
-  layer = 1;
+  origImage = await loadImageAsync(path);
+  coloredImage = createImage(origImage.width, origImage.height)
+  coloredImage.copy(origImage, 0, 0, origImage.width, origImage.height, 0, 0, coloredImage.width, coloredImage.height)
+  prepareImages(origImage, coloredImage)
+  currentLayer = 1;
   startTime = Date.now()
   loop()
 }
 
 window.preload = () => {
   name = "input_image.png"
-  img = loadImage(name)
-  copy = loadImage(name)
+  origImage = loadImage(name)
+  coloredImage = loadImage(name)
 }
 
 window.setup = () => {
+  currentLayer = 0;
   startTime = Date.now()
-  prepareImages(img, copy);
+  prepareImages(origImage, coloredImage);
   colorsAt = getLayerInfo();
-  if (typeof img == 'undefined' || typeof copy == 'undefined' || !img.pixels || !copy.pixels) {
+  if (typeof origImage == 'undefined' || typeof coloredImage == 'undefined' || !origImage.pixels || !coloredImage.pixels) {
     noLoop()
   } else {
     loop();
@@ -46,20 +49,14 @@ window.setup = () => {
 }
 
 window.draw =() => {
-  if (layer > layers || !colorsAt) {
-    console.log("Total took " + (Date.now() - startTime) / 1000)
-    noLoop();
-    return;
-  }
-
   let time = Date.now()
-  let thresh = parseInt(subd * (layer - 1)) // threshold to change pixels
-  let copyData = copy.pixels
-  let imgData = img.pixels
+  let thresh = parseInt(subd * (currentLayer - 1)) // threshold to change pixels
+  let copyData = coloredImage.pixels
+  let imgData = origImage.pixels
 
-  for (let currentLayer in colorsAt) {
-    if (layer <= currentLayer) {
-      targetColor = colorsAt[currentLayer];
+  for (let selectedLayer in colorsAt) {
+    if (currentLayer <= selectedLayer) {
+      selectedColor = colorsAt[selectedLayer];
       break;
     }
   }
@@ -68,18 +65,23 @@ window.draw =() => {
     if (imgData[i + 3] == 0) continue; // skip on transparent pixels for png files
 
     currentValue = (imgData[i] + imgData[i + 1] + imgData[i + 2]) / 3
-
     if (currentValue >= thresh) {
       origColor = color(copyData[i], copyData[i + 1], copyData[i + 2])
-      colorLayer(targetColor)
+      colorLayer(selectedColor)
     }
   }
 
-  console.log("Layer " + layer + ", with " + targetColor + ", taking: " + (Date.now() - time) / 1000.0 + "s")
-  copy.updatePixels()
-  image(copy, 0, 0)
-  image(img, img.width + 20, 0)
-  layer++
+  console.log("current layer " + currentLayer + ", with " + selectedColor + ", taking: " + (Date.now() - time) / 1000.0 + "s")
+  coloredImage.updatePixels()
+  image(coloredImage, 0, 0)
+  image(origImage, origImage.width + 20, 0)
+  currentLayer++
+
+  if (currentLayer > layers || !colorsAt) {
+    console.log("Total took " + (Date.now() - startTime) / 1000)
+    noLoop();
+    return;
+  }
 }
 
 document.getElementById("fileInput").addEventListener("change", handleImageInput)
@@ -90,7 +92,7 @@ function colorLayer(currentColor) {
     destColor = color(currentColor[0])
   }
   let result = mixbox.lerp(origColor.levels, destColor.levels, currentColor[1]) // color mix using mixbox for accurate pigment mixing results
-  copy.pixels[i + 0] = result[0]
-  copy.pixels[i + 1] = result[1]
-  copy.pixels[i + 2] = result[2]
+  coloredImage.pixels[i + 0] = result[0]
+  coloredImage.pixels[i + 1] = result[1]
+  coloredImage.pixels[i + 2] = result[2]
 }
