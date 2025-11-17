9bdaf13a2974478197276fabfa74f0fafcfadd35
LucasKendi <hiranolucas@gmail.com>
2025-11-16 21:18:01 -0300

    Variables cleanup

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 p5.disableFriendlyErrors = true; //small performance boost
 import { getLayerInfo, prepareImages, loadImageAsync, buildAndMixColors } from './image_processor.js';
 
-let origImage;
-let coloredImage;
-let range = 255;
+let origImage, coloredImage, startTime;
 let layers = 15;
 let currentLayer = 0;
-let subd = parseInt(range / layers);
-let previousColor = [];
-let destColor = '';
-let startTime;
-let colorsAt;
+let subdivisions = parseInt(255 / layers);
+let inputColors = getLayerInfo();
 let selectedColor;
-let currentValue;
-let origColor;
-let i;
 
 window.handleImageInput = async (event) => {
   let path = event.target.files[0];
@@ -24,23 +16,20 @@ window.handleImageInput = async (event) => {
   origImage = await loadImageAsync(path);
   coloredImage = createImage(origImage.width, origImage.height)
   coloredImage.copy(origImage, 0, 0, origImage.width, origImage.height, 0, 0, coloredImage.width, coloredImage.height)
-  prepareImages(origImage, coloredImage)
-  currentLayer = 1;
-  startTime = Date.now()
-  loop()
+  setup()
 }
 
 window.preload = () => {
-  name = "input_image.png"
-  origImage = loadImage(name)
-  coloredImage = loadImage(name)
+  let inputImage = "input_image.png"
+  origImage = loadImage(inputImage)
+  coloredImage = loadImage(inputImage)
 }
 
 window.setup = () => {
   currentLayer = 0;
   startTime = Date.now()
   prepareImages(origImage, coloredImage);
-  colorsAt = getLayerInfo();
+
   if (typeof origImage == 'undefined' || typeof coloredImage == 'undefined' || !origImage.pixels || !coloredImage.pixels) {
     noLoop()
   } else {
@@ -50,33 +39,37 @@ window.setup = () => {
 
 window.draw = () => {
   let time = Date.now()
-  let thresh = parseInt(subd * (currentLayer - 1)) // threshold to change pixels
-  let copyData = coloredImage.pixels
-  let imgData = origImage.pixels
+  let threshold = parseInt(subdivisions * (currentLayer - 1)) // threshold to change pixels
+  let coloredPixels = coloredImage.pixels
+  let origPixels = origImage.pixels
 
   if (selectedColor) {
-    if (colorsAt[currentLayer]) { selectedColor = colorsAt[currentLayer] }
+    if (inputColors[currentLayer]) { selectedColor = inputColors[currentLayer] }
   } else {
-    selectedColor = colorsAt[Object.keys(colorsAt)[0]]
+    selectedColor = inputColors[Object.keys(inputColors)[0]]
   }
 
-  for (i = 0; i < imgData.length; i += 4) {
-    if (imgData[i + 3] == 0) continue; // skip transparent pixels on png files
+  for (let i = 0; i < origPixels.length; i += 4) {
+    if (origPixels[i + 3] == 0) continue; // skip transparent pixels on png files
 
-    currentValue = (imgData[i] + imgData[i + 1] + imgData[i + 2]) / 3
-    if (currentValue >= thresh) {
-      origColor = color(copyData[i], copyData[i + 1], copyData[i + 2])
-      colorLayer(selectedColor)
+    let currentValue = (origPixels[i] + origPixels[i + 1] + origPixels[i + 2]) / 3
+    if (currentValue >= threshold) {
+      let origColor = color(Array.from(coloredPixels.slice(i, i + 3)))
+      let result = buildAndMixColors(origColor, selectedColor)
+
+      coloredImage.pixels[i + 0] = result[0]
+      coloredImage.pixels[i + 1] = result[1]
+      coloredImage.pixels[i + 2] = result[2]
     }
   }
 
-  console.log("current layer " + currentLayer + ", with " + selectedColor + ", taking: " + (Date.now() - time) / 1000.0 + "s")
+  console.log("current layer " + currentLayer + ", with " + selectedColor["color"] + ", taking: " + (Date.now() - time) / 1000.0 + "s")
   coloredImage.updatePixels()
   image(coloredImage, 0, 0)
   image(origImage, origImage.width, 0)
   currentLayer++
 
-  if (currentLayer > layers || !colorsAt) {
+  if (currentLayer > layers || !inputColors) {
     console.log("Total took " + (Date.now() - startTime) / 1000)
     noLoop();
     return;
@@ -84,10 +77,3 @@ window.draw = () => {
 }
 
 document.getElementById("fileInput").addEventListener("change", handleImageInput)
-
-function colorLayer(currentColor) {
-  let result = buildAndMixColors(origColor, currentColor)
-  coloredImage.pixels[i + 0] = result[0]
-  coloredImage.pixels[i + 1] = result[1]
-  coloredImage.pixels[i + 2] = result[2]
-}
