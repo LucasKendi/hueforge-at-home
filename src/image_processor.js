8531d882a8126d8bcb38bdfa166f2b57267aa375
LucasKendi <hiranolucas@gmail.com>
2025-11-20 20:56:01 -0300

    Massive gain in performance and gradient preview

--- src/image_processor.js
+++ src/image_processor.js
@@ -33,5 +33,23 @@ export function loadImageAsync(filePath) {
 
 export function buildAndMixColors(baseColor, selectedColor) {
   let mixColor = color(selectedColor["color"])
-  return mixbox.lerp(baseColor.levels, mixColor.levels, selectedColor["opacity"]) // color mix using mixbox for accurate pigment mixing results
+  return mixbox.lerp(color(baseColor).levels, mixColor.levels, selectedColor["opacity"]) // color mix using mixbox for accurate pigment mixing results
+}
+
+export function paintPreview(image, colors) {
+  let data = image.data;
+  let ignore = parseInt(image.width/colors.length)
+  for (let i = 0; i < colors.lenght; i++) {
+    for (let x = 0; x < image.width; x++) {
+      if (i * ignore > x) { continue }
+      for (let y = 0; y < image.height; y++) {
+        let coord = (y * image.width + x) * 4;
+        image.data[coord + 0] = colors[i][0]
+        image.data[coord + 1] = colors[i][1]
+        image.data[coord + 2] = colors[i][2]
+        image.data[coord + 3] = 255
+      }
+    }
+  }
+  return data
 }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import { getLayerInfo, prepareImages, loadImageAsync, buildAndMixColors } from '
 
 let origImage, coloredImage, startTime;
 let layers = 15;
-let currentLayer = 0;
+let currentLayer;
 let subdivisions = parseInt(255 / layers);
 let inputColors = getLayerInfo();
-let selectedColor;
+let layerColors = [];
+
+let gradientPreview = document.getElementById('gradient-preview').getContext("2d")
+let previewX = 450, previewY = 150;
+let step = parseInt(previewX / layers);
+
+let imgData, dataArray;
 
 window.handleImageInput = async (event) => {
   let path = event.target.files[0];
@@ -26,47 +32,56 @@ window.preload = () => {
 }
 
 window.setup = () => {
-  currentLayer = 0;
   startTime = Date.now()
+  currentLayer = 0;
   prepareImages(origImage, coloredImage);
+  imgData = gradientPreview.createImageData(previewX, previewY);
+  dataArray = imgData.data;
+
+  let currentColor = inputColors[Object.keys(inputColors)[0]]
+  layerColors[0] = color(currentColor["color"])
+
+  for (let i = 1; i <= layers; i++) {
+    if (inputColors[i]) { currentColor = inputColors[i] }
+
+    layerColors[i] = buildAndMixColors(layerColors[i - 1], currentColor)
+  }
 
   if (typeof origImage == 'undefined' || typeof coloredImage == 'undefined' || !origImage.pixels || !coloredImage.pixels) {
     noLoop()
-  } else {
-    loop();
-  }
+  } else { loop() }
 }
 
 window.draw = () => {
-  let time = Date.now()
   let threshold = parseInt(subdivisions * (currentLayer - 1)) // threshold to change pixels
-  let coloredPixels = coloredImage.pixels
   let origPixels = origImage.pixels
-
-  if (selectedColor) {
-    if (inputColors[currentLayer]) { selectedColor = inputColors[currentLayer] }
-  } else {
-    selectedColor = inputColors[Object.keys(inputColors)[0]]
-  }
 
   for (let i = 0; i < origPixels.length; i += 4) {
     if (origPixels[i + 3] == 0) continue; // skip transparent pixels on png files
-
     let currentValue = (origPixels[i] + origPixels[i + 1] + origPixels[i + 2]) / 3
     if (currentValue >= threshold) {
-      let origColor = color(Array.from(coloredPixels.slice(i, i + 3)))
-      let result = buildAndMixColors(origColor, selectedColor)
-
-      coloredImage.pixels[i + 0] = result[0]
-      coloredImage.pixels[i + 1] = result[1]
-      coloredImage.pixels[i + 2] = result[2]
+      coloredImage.pixels[i + 0] = layerColors[currentLayer][0]
+      coloredImage.pixels[i + 1] = layerColors[currentLayer][1]
+      coloredImage.pixels[i + 2] = layerColors[currentLayer][2]
     }
   }
 
-  console.log("current layer " + currentLayer + ", with " + selectedColor["color"] + ", taking: " + (Date.now() - time) / 1000.0 + "s")
+  for (let x = 0; x < previewX; x++) {
+    if (currentLayer * step > x) { continue }
+    for (let y = 0; y < previewY; y++) {
+      let i = (y * previewX + x) * 4;
+
+      dataArray[i + 0] = layerColors[currentLayer][0];
+      dataArray[i + 1] = layerColors[currentLayer][1];
+      dataArray[i + 2] = layerColors[currentLayer][2];
+      dataArray[i + 3] = 255;
+    }
+  }
+
   coloredImage.updatePixels()
   image(coloredImage, 0, 0)
   image(origImage, origImage.width, 0)
+  gradientPreview.putImageData(imgData, 0, 0)
   currentLayer++
 
   if (currentLayer > layers || !inputColors) {
